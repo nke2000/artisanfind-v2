ce79ed001acb4ca279100c1c047b302e2112f7af
nke2000 <kouaoelysee3@gmail>
2025-06-17 07:54:05 +0000

    modifier projet

--- annuaireapp/admin.py
+++ annuaireapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Artisan, ContactMessage
 
-# Register your models here.
+@admin.register(Artisan)
+class ArtisanAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'email', 'telephone', 'ville', 'metier', 'experience')
+    list_filter = ('metier', 'ville')
+    search_fields = ('nom', 'email', 'ville', 'metier')
+    readonly_fields = ('mot_de_passe',)  # Pour éviter la modification directe
+    fieldsets = (
+        ("Informations personnelles", {
+            'fields': ('nom', 'email', 'telephone', 'ville', 'mot_de_passe')
+        }),
+        ("Détails professionnels", {
+            'fields': ('metier', 'experience', 'description', 'photos')
+        }),
+        ("Localisation", {
+            'fields': ('localisation',)
+        }),
+    )
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'email', 'date_envoi')
+    search_fields = ('nom', 'email', 'message')
+    readonly_fields = ('nom', 'email', 'message', 'date_envoi')
+    ordering = ('-date_envoi',)
